bd4ee75bc40a91da47b57f8a424b84e1d9b7193c
aman0302 <aman.vice@gmail.com>
2017-11-04 18:50:05 +0530

    change selenium browser to chrome

--- logger/action/BrowserFactory.py
+++ logger/action/BrowserFactory.py
@@ -5,6 +5,6 @@ from logger.utils.ExecutablePath import *
 class browser_factory():
 
     def get_browser(self):
-        #browser = webdriver.Chrome(executable_path=get_chromedriver_path())
-        browser = webdriver.Firefox(executable_path=get_geckodriver_path())
+        browser = webdriver.Chrome(executable_path=get_chromedriver_path())
+        #browser = webdriver.Firefox(executable_path=get_geckodriver_path())
         return browser
